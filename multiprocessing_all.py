e38ce5fc03be675256921457245e931155bb7a58
stal stalk <pratikbelote77@gmail.com>
2023-12-12 11:57:25 +0530

    multiprocessing all codes

---
+++ multiprocessing_all.py
@@ -0,0 +1,157 @@
+import time
+import multiprocessing
+
+def calc_square(numbers):
+    for n in numbers:
+        print('square ' + str(n*n))
+
+def calc_cube(numbers):
+    for n in numbers:
+        print('cube ' + str(n*n*n))
+
+if __name__ == "__main__":
+    arr = [2,3,8]
+    p1 = multiprocessing.Process(target=calc_square, args=(arr,))
+    p2 = multiprocessing.Process(target=calc_cube, args=(arr,))
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    print("Done!")
+
+...
+import time
+import multiprocessing
+
+def deposit(balance, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        balance.value = balance.value + 1
+        lock.release()
+
+
+
+def withdraw(balance, lock):
+    for i in range(100):
+        time.sleep(0.01)
+        lock.acquire()
+        balance.value = balance.value - 1
+        lock.release()
+
+if __name__ == '__main__':
+    balance = multiprocessing.Value('i', 200)
+    lock = multiprocessing.Lock()
+    d = multiprocessing.Process(target=deposit, args=(balance,lock))
+    w = multiprocessing.Process(target=withdraw, args=(balance,lock))
+    d.start()
+    w.start()
+    d.join()
+    w.join()
+    print(balance.value)
+
+
+
+from multiprocessing import Pool
+def f(n):
+    return n*n
+
+if __name__ == "__main__":
+    p = Pool(processes=3)
+    result = p.map(f,[1,2,3,4,5])
+    for n in result:
+        print(n)
+
+
+
+import multiprocessing
+
+def calc_square(numbers, q):
+    for n in numbers:
+        q.put(n*n)
+
+if __name__ == "__main__":
+    numbers = [2,3,5]
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=calc_square, args=(numbers,q))
+
+    p.start()
+    p.join()
+
+    while q.empty() is False:
+        print(q.get())
+
+import multiprocessing
+
+def calc_square(numbers, result, v):
+    v.value = 5.67
+    for idx, n in enumerate(numbers):
+        result[idx] = n*n
+
+if __name__ == "__main__":
+    numbers = [2,3,5]
+    result = multiprocessing.Array('i',3)
+    v = multiprocessing.Value('d', 0.0)
+    p = multiprocessing.Process(target=calc_square, args=(numbers, result, v))
+
+    p.start()
+    p.join()
+
+    print(v.value)
+
+import time
+from random import randint
+import threading
+
+queue = []
+
+def produce():
+    for i in range(0,5):
+        time.sleep(1)
+        queue.append(randint(0,9))
+
+def consume():
+    while True:
+        if len(queue) > 0:
+            
+
+if "__name__"=="__main__":
+    p = threading.Thread(target=produce)
+    c = threading.Thread(target=consume)
+
+import time
+import threading
+
+def calc_square(numbers):
+    print("calculate square numbers")
+    for n in numbers:
+        time.sleep(1)
+        print('square:',n*n)
+
+def calc_cube(numbers):
+    print("calculate cube of numbers")
+    for n in numbers:
+        time.sleep(1)
+        print('cube:',n*n*n)
+
+arr = [2,3,8,9]
+
+t = time.time()
+
+t1= threading.Thread(target=calc_square, args=(arr,))
+t2= threading.Thread(target=calc_cube, args=(arr,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("done in : ",time.time()-t)
+print("Hah... I am done with all my work now!")
+
+
+...
